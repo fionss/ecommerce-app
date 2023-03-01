0b14bd0af634100ea4da7f878f5dc3f452a6969d
Fiona Attah <princessfiona111@gmail.com>
2023-02-28 23:20:12 -0500

    new changes

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./images/1.png";
+import product2 from "./images/2.png";
+import product3 from "./images/3.png";
+import product4 from "./images/4.png";
+import product5 from "./images/5.png";
+import product6 from "./images/6.png";
+import product7 from "./images/7.png";
+import product8 from "./images/8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Fujifilm",
+    price: '$99',
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Hoodie",
+    price: '$80',
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Camera",
+    price: '$200',
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Ipad",
+    price: '$400',
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "IPhone",
+    price: '$850',
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Laptop",
+    price: '$1200',
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Sneakers",
+    price: '$85',
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "HeadPhones",
+    price: '$400',
+    productImage: product8,
+  },
+]
